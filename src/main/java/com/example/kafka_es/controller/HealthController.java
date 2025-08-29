e3ee176986abec888845699ec06c63004213fa02
JungHyunLee <aktlakfhjh2@gmail.com>
2025-08-30 01:10:05 +0900

    add health check

---
+++ src/main/java/com/example/kafka_es/controller/HealthController.java
@@ -0,0 +1,12 @@
+package com.example.kafka_es.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+    @GetMapping("/health")
+    public String health() {
+        return "OK";
+    }
+}
